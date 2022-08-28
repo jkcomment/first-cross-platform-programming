d9229f49849353a3ee4d3267494c0e544adba1f2
jkcomment <jkcomment@gmail.com>
2022-08-28 20:19:54 +0900

    impl add and sub for rust-cross-ios

--- rust-cross-ios/src/lib.rs
+++ rust-cross-ios/src/lib.rs
@@ -1,3 +1,13 @@
+#[no_mangle]
+pub extern "C" fn add(a: i64, b: i64) -> i64 {
+    rust_cross::add(a, b)
+}
+
+#[no_mangle]
+pub extern "C" fn sub(a: i64, b: i64) -> i64 {
+    rust_cross::sub(a, b)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
