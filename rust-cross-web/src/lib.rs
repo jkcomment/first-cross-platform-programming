833acbe4b41a62f56c940d9d021e5ff63f2e98b5
jkcomment <jkcomment@gmail.com>
2022-08-28 20:31:58 +0900

    ロジック追加

--- rust-cross-web/src/lib.rs
+++ rust-cross-web/src/lib.rs
@@ -1,3 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(a: i64, b: i64) -> i64 {
+    rust_cross::add(a, b)
+}
+
+#[wasm_bindgen]
+pub fn sub(a: i64, b: i64) -> i64 {
+    rust_cross::sub(a, b)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
